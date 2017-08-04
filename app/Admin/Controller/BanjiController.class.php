7b2825de7e4b6a5598e2fb9939fc12d0931c6491
zhangtengrui <645605935@qq.com>
2017-08-04 15:49:17 +0800

    32131

--- app/Admin/Controller/BanjiController.class.php
+++ app/Admin/Controller/BanjiController.class.php
@@ -85,6 +85,54 @@ class BanjiController extends AuthController {
         $this->display();
     }
 
+    //线尾图片
+    public function index_5(){
+        global $user;
+        $this->cur_v='Banji-index_5';
+
+        $page="Card/index_5";
+        $page_buttons=M('PageButtons')->where(array('page'=>$page))->select();
+        $this->page_buttons=$page_buttons;
+        $this->page=$page;
+        
+        $this->display();
+    }
+
+
+    // 上传图片
+    public function lay_upload_file(){
+        if($_FILES['img']['size']>0){
+            $upload = new \Think\Upload();// 实例化上传类
+            $upload->maxSize   =     31457280 ;// 设置附件上传大小
+            $upload->exts      =     array('zip', 'rar', 'xls','xlsx', 'doc','docx','ppt','pptx','pdf','jpg','png','jpeg','gif');// 设置附件上传类型
+            $upload->uploadReplace  = false;// 存在同名文件是否覆盖
+            $upload->autoSub   =     false;//是否启用子目录保存
+            $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+            $upload->savePath  =     'layui/'; // 设置附件上传（子）目录
+            $upload->saveRule  =     ''; // 设置附件上传（子）目录
+             
+            // 上传文件 
+            $info   =   $upload->upload();
+
+            if(!$info) {
+                $data=array();
+                $data['code']=0;
+                $data['msg']=$upload->getError();
+            }else{
+                $img=$info['img']['savename'];
+                $data=array();
+                $data['code']=0;
+                $data['msg']='success';
+                $data['data']["src"]='/Uploads/layui/'.$img;
+            }
+
+            //上传到阿里云OSS
+            // oss_upload( '/Uploads/layui/'.$img );
+
+            echo json_encode($data);
+        }
+    }
+
 
 }
 
