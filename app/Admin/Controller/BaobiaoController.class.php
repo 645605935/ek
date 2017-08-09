49ff21301bdad51f748b402196e948764cbc5d3f
zhangtengrui <645605935@qq.com>
2017-08-09 16:40:00 +0800

    no message

---
+++ app/Admin/Controller/BaobiaoController.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AuthController;
+
+class BaobiaoController extends AuthController {
+	
+    public function _initialize() {
+        parent::_initialize();
+        global $user;
+        $user=session('auth');
+        $this->user=$user;
+        $this->cur_c='Baobiao';
+
+        $group=M('auth_group')->where(array('pid'=>0))->select();
+        foreach ($group as $key => $value) {
+            $group[$key]['_child']=M('auth_group')->where(array('pid'=>$value['id']))->select();
+        }
+        $this->group=$group;
+    }
+
+    public function index_1(){
+        global $user;
+        $this->cur_v='Baobiao-index_1';
+
+        
+        $this->display();
+    }
+
+    
+
+   
+}
+
+?>
