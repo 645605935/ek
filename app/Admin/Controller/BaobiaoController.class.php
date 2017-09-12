6b1149d24cc5ee2b9ee911bb6ea2c3960d96f57e
zhangtengrui <645605935@qq.com>
2017-09-12 15:20:58 +0800

    no message

--- app/Admin/Controller/BaobiaoController.class.php
+++ app/Admin/Controller/BaobiaoController.class.php
@@ -82,6 +82,13 @@ class BaobiaoController extends AuthController {
         $this->display();
     }
 
+    public function index_4_6(){
+        global $user;
+        $this->cur_v='Baobiao-index_4_6';
+        
+        $this->display();
+    }
+
 
     public function index_5(){
         global $user;
